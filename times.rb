366669cd552f3abb0224133ae73dc4a1ca16bda8
Christopher Holloway <mrcholloway@yahoo.com>
2020-07-19 19:38:21 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,15 +1,5 @@
 def using_times
-  
-  
-  
-  
-  
-  
-  
-  
-  def using_times 
-	7.times do
-		puts "Wingardium Leviosa"
-	end
+  7.times do
+    puts "Wingardium Leviosa"
+  end
 end
-using_times
